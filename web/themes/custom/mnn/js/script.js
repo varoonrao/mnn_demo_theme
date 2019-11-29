59ed0a22f478254f2594c5473a2a2905bd9de9c9
varoonrao <varunrao72@gmail.com>
2019-11-29 18:37:32 +0530

    6:37pm

---
+++ web/themes/custom/mnn/js/script.js
@@ -0,0 +1,11 @@
+// jQuery(document).ready(function($){
+//     $('block-mnn-main-menu li a').hover(function(){
+//         $(this).parent().find('.sub_menu').toggle();
+//     });
+// });
+
+function toggle(){
+    let tog=document.getElementsByClassName('sub-menu');
+    tog.display="block";
+    console.log("hello");
+}
